669c24b304436da6b0ff8c136e2cf21f658456fc
MikolajOgar <mikolajogarek@gmail.com>
2019-05-02 15:57:15 +0200

    board.py changes, comments and refactoring

--- board.py
+++ board.py
@@ -2,30 +2,37 @@ import pygame
 
 class Board(object):
     """
-    Plansza do gry. Odpowiada za rysowanie okna gry.
+    Game board responsible for:
+    providing window(__init__)
+    drawing(draw)
+
     """
 
     def __init__(self, width, height):
         """
-        Konstruktor planszy do gry. Przygotowuje okienko gry.
+        Constructor for board.
+        Prepares window for the game.
 
-        :param width: szerokość w pikselach
-        :param height: wysokość w pikselach
+        :param width: width in pixels
+        :param height: height in pixels
+
         """
         self.surface = pygame.display.set_mode((width, height), 0, 32)
         pygame.display.set_caption('Game of life')
 
     def draw(self, *args):
         """
-        Rysuje okno gry
+        Draws the window for the game
 
-        :param args: lista obiektów do narysowania
+        :param args: list of object to draw
         """
+
+        # seting black background
         background = (0, 0, 0)
         self.surface.fill(background)
+
         for drawable in args:
             drawable.draw_on(self.surface)
 
-        # dopiero w tym miejscu następuje fatyczne rysowanie
-        # w oknie gry, wcześniej tylko ustalaliśmy co i jak ma zostać narysowane
+        #updating the display and making visible changes on the screen
         pygame.display.update()
